2560637b1abd941f0d228c5f950383980ebbc693
hdhdhhdhdhdhd <149946965+hdhdhhdhdhdhd@users.noreply.github.com>
2026-04-01 17:08:45 +0700

    Update app to run on specified port from environment

--- app.py
+++ app.py
@@ -34,5 +34,8 @@ def handle_message(event):
         TextSendMessage(text="คุณพิมพ์: " + text)
     )
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
