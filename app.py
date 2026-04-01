e760de51d8756d8b298cb41eef834eecba4cb4e3
hdhdhhdhdhdhd <149946965+hdhdhhdhdhdhd@users.noreply.github.com>
2026-04-01 16:18:03 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.exceptions import InvalidSignatureError
+import os
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(os.getenv("ACCESS_TOKEN"))
+handler = WebhookHandler(os.getenv("CHANNEL_SECRET"))
+
+@app.route("/")
+def home():
+    return "Bot Running!"
+
+@app.route("/webhook", methods=['POST'])
+def webhook():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    text = event.message.text
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="คุณพิมพ์: " + text)
+    )
+
+if __name__ == "__main__":
+    app.run()
